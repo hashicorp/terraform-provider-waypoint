5096e93b2ca4270b6d2bade636df0a27ee55ef79
Cassie Coyle <25733780+cicoyle@users.noreply.github.com>
2023-06-01 14:08:35 -0500

    t.Errorf instead of new package

--- internal/provider/config_source_resource_test.go
+++ internal/provider/config_source_resource_test.go
@@ -1,16 +1,16 @@
 package provider
 
 import (
-	"github.com/stretchr/testify/require"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
 func TestAccConfigSource(t *testing.T) {
-	require := require.New(t)
 	tfConfig, err := helperTestAccTFExampleConfig("resources/waypoint_config_source/resource.tf")
-	require.NoError(err)
+	if err != nil {
+		t.Errorf("error reading config from file: %s", err)
+	}
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheck(t) },
